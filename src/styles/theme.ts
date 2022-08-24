26ddc44aa6db9cbbad176a1b804b9fab3f0ca609
Circlewee <pnj1118@naver.com>
2022-08-25 00:09:10 +0900

    feat: darkBlue, white 추가 / font: medium, bold 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,13 @@ import { DefaultTheme } from 'styled-components';
 
 const theme: DefaultTheme = {
   color: {
+    darkBlue: '#1537B1',
     orange: '#FF7A00',
+    white: '#F8FCFF',
+  },
+  font: {
+    medium: 'GmarketSansMedium',
+    bold: 'GmarketSansBold',
   },
 };
 
--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -3,7 +3,13 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     color: {
+      darkBlue: string;
       orange: string;
+      white: string;
+    };
+    font: {
+      medium: string;
+      bold: string;
     };
   }
 }
