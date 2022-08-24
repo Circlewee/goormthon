fa8a8c364a9a4c37ce84e398d5b342eb2c3ab70e
Circlewee <pnj1118@naver.com>
2022-08-25 00:10:06 +0900

    style: Header 스타일 수정

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
+  background-color: ${({ theme }) => theme.color.white};
   text-align: center;
   color: ${(props) => props.theme.color.orange};
 `;
 
 export const Title = styled.h1`
+  padding: 60px 0 0 0;
+  margin: 0;
   font-family: 'GmarketSansBold';
   font-size: 42px;
   font-weight: bold;
+  line-height: 42px;
 `;
 
 export const SubTitle = styled.h2`
+  margin: 0;
   font-size: 16px;
+  line-height: 42px;
 `;
