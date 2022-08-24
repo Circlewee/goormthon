e56c0d613dcc213524e68342583d12b41821207c
Circlewee <pnj1118@naver.com>
2022-08-24 18:42:34 +0900

    style: GlobalStyle, Header style 수정

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  text-align: center;
+  color: ${(props) => props.theme.color.orange};
+`;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+  font-family: 'GmarketSansBold';
+  font-size: 42px;
+  font-weight: bold;
+`;
 
-export const SubTitle = styled.h2``;
+export const SubTitle = styled.h2`
+  font-size: 16px;
+`;
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,24 +2,33 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
-    font-family: 'MICEGothic';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2206-01@1.0/MICEGothic.woff2') format('woff2');
-    font-weight: 400;
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'GmarketSansBold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff') format('woff');
+    font-weight: 700;
     font-style: normal;
   }
 
   *, *::before, *::after {
     box-sizing: border-box;
-    font-family: 'MICEGothic',sans-serif;
+    font-family: 'GmarketSansMedium', sans-serif;
   }
 
   body {
     margin: 0;
     display: flex;
     justify-content: center;
-    
+    align-items: center;
+  }
+  
+  body > div {
     width: 100%;
-    max-width: 100%;
+    max-width: 500px;
   }
 
   a {
@@ -30,7 +39,7 @@ const GlobalStyle = createGlobalStyle`
   input,
   button,
   textarea {
-    font-family: 'MICEGothic',sans-serif;
+    font-family: 'GmarketSansMedium', sans-serif;
     background-color: transparent;
     border: none;
     outline: none;
