a69773092a19448ebbe43f27598f9be0d9573df5
Circlewee <pnj1118@naver.com>
2022-08-25 00:10:41 +0900

    feat: Nav 완성

--- src/components/Nav/Nav.style.ts
+++ src/components/Nav/Nav.style.ts
@@ -1,7 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Nav = styled.nav`
   width: 100%;
+  max-width: 500px;
+  padding: 16px 56px 0 56px;
+  background-color: ${({ theme }) => theme.color.white};
+
+  > a {
+    width: 50%;
+    display: inline-block;
+  }
 `;
 
-export const LinkButton = styled.button``;
+export const LinkButton = styled.button<{ selected: boolean }>`
+  width: 100%;
+  font-size: 18px;
+  padding: 9px 0;
+  color: ${({ theme }) => theme.color.darkBlue};
+
+  ${({ selected, theme }) =>
+    selected &&
+    css`
+      font-family: ${({ theme }) => theme.font.bold}, sans-serif;
+      border-radius: 4px 4px 0px 0px;
+      color: #fff;
+      background-color: ${theme.color.darkBlue};
+    `};
+`;
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import * as SC from './Nav.style';
 
 const Nav = () => {
+  const location = useLocation();
+
   return (
     <SC.Nav>
       <Link to='/name'>
-        <SC.LinkButton>이름</SC.LinkButton>
+        <SC.LinkButton selected={location.pathname === '/name'}>이름</SC.LinkButton>
       </Link>
       <Link to='/birthday'>
-        <SC.LinkButton>생일</SC.LinkButton>
+        <SC.LinkButton selected={location.pathname === '/birthday'}>생일</SC.LinkButton>
       </Link>
     </SC.Nav>
   );
