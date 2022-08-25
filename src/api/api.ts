0af297395c57d094524af806cf184433eb6c05a2
Circlewee <pnj1118@naver.com>
2022-08-25 19:05:56 +0900

    postTransfer

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,4 +4,9 @@ const customAxios = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+export const postTransfer = async (meanArray: string[]) => {
+  const data = await customAxios.post('/transfer', meanArray);
+  console.log(data);
+};
+
 export default customAxios;
