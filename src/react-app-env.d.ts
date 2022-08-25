7659d9d6109c29c0f64df36e179ce19884188097
Circlewee <pnj1118@naver.com>
2022-08-25 13:13:59 +0900

    feat: Window.Kakao 타입 추가

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,5 +4,10 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production';
     readonly REACT_APP_API_URL: string;
+    readonly REACT_APP_KAKAO_KEY: string;
   }
+}
+
+interface Window {
+  Kakao: any;
 }
