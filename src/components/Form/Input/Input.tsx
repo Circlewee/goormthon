f4c1cfce68e120fdc00723377e6e427bc59e9a5c
Circlewee <pnj1118@naver.com>
2022-08-25 15:09:11 +0900

    feat: dynamic form에 react-hook-form 라이브러리 적용

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -1,14 +1,16 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+
 import * as SC from './Input.style';
 
 interface InputProps {
   id: string;
-  name: string;
   placeholder: string;
   width?: number;
+  register?: UseFormRegisterReturn;
 }
 
-const Input = ({ id, name, placeholder, width }: InputProps) => {
-  return <SC.Input id={id} name={name} placeholder={placeholder} width={width} />;
+const Input = ({ id, placeholder, width, register }: InputProps) => {
+  return <SC.Input id={id} placeholder={placeholder} width={width} {...register} />;
 };
 
 export default Input;
--- src/pages/NamePage/NamePage.tsx
+++ src/pages/NamePage/NamePage.tsx
@@ -1,30 +1,48 @@
-import { useState, useRef } from 'react';
-import { nanoid } from 'nanoid';
+import { useForm, useFieldArray } from 'react-hook-form';
 
 import * as SC from './NamePage.style';
 import { Label } from '../../components/Form/Label';
 import { Input } from '../../components/Form/Input';
 
-const NamePage = () => {
-  const [extraInput, setExtraInput] = useState(1);
-  const formRef = useRef<HTMLFormElement>(null);
+type FormType = {
+  name: {
+    mean: string;
+  }[];
+};
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const nameData: string[] = [];
-    for (let i = 0; i < e.currentTarget.length; i++) {
-      if (e.currentTarget[i] instanceof HTMLInputElement) {
-        // nameData.push(e.currentTarget[i].value);
-      }
-    }
+const NamePage = () => {
+  // const [firstInput, setFirstInput] = useState('');
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormType>({
+    mode: 'onChange',
+    defaultValues: {
+      name: [{ mean: '' }, { mean: '' }],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    name: 'name',
+    control,
+  });
+
+  const abc = (data: FormType) => {
+    const meanArray = data.name.map((name) => name.mean);
+
+    console.log(meanArray);
   };
 
   const handleInputAdd = () => {
-    setExtraInput((prevState) => prevState + 1);
+    append({ mean: '' });
   };
 
-  const handleInputDelete = () => {
-    setExtraInput((prevState) => prevState - 1);
+  const handleInputDelete = (index: number) => {
+    return () => {
+      remove(index);
+    };
   };
 
   return (
@@ -33,44 +51,41 @@ const NamePage = () => {
         <SC.RealNameContainer>
           <div>
             <Label htmlFor='lastName'>성</Label>
-            <Input id='lastName' placeholder='성' name='lastName' width={98} />
+            <Input id='lastName' placeholder='성' width={98} />
           </div>
           <div>
             <Label htmlFor='firstName'>이름</Label>
-            <Input id='firstName' placeholder='이름' name='firstName' />
+            <Input id='firstName' placeholder='이름' />
           </div>
         </SC.RealNameContainer>
-        <SC.Form onSubmit={handleSubmit} ref={formRef}>
+        <SC.Form onSubmit={handleSubmit(abc)}>
           <SC.ExplanationText>
             이름의 의미를 동사로 작성해주세요.
             <br />
             (빛날)희 (착할)선 &gt; 빛나다, 착하다
           </SC.ExplanationText>
-          <SC.NameMeanContainer>
-            <Label htmlFor='meanInputFirst'>1번째 의미</Label>
-            <Input id='meanInputFirst' placeholder='빛나다' name='meanInputFirst' />
-          </SC.NameMeanContainer>
-          {Array(extraInput)
-            .fill(0)
-            .map((_, index) => {
-              return (
-                <SC.NameMeanContainer key={nanoid()}>
-                  <Label htmlFor={`meanInput${index + 2}`}>{index + 2}번째 의미</Label>
-                  <SC.InputWrapper>
-                    <Input
-                      id={`meanInput${index + 2}`}
-                      placeholder='착하다'
-                      name={`meanInput${index + 2}`}
-                    />
-                    {index === extraInput - 1 && (
-                      <SC.DeleteButton type='button' onClick={handleInputDelete}>
-                        -
-                      </SC.DeleteButton>
-                    )}
-                  </SC.InputWrapper>
-                </SC.NameMeanContainer>
-              );
-            })}
+          {fields.map((field, index) => {
+            return (
+              <SC.NameMeanContainer key={field.id}>
+                <Label htmlFor={`meanInput${index}`}>{index + 1}번째 의미</Label>
+                <SC.InputWrapper>
+                  <Input
+                    id={`meanInput${index}`}
+                    placeholder='착하다'
+                    register={register(`name.${index}.mean` as const, {
+                      required: index === 0 && true,
+                    })}
+                  />
+                  {errors.name?.message && <div>필수입력</div>}
+                  {fields.length !== 1 && index === fields.length - 1 && (
+                    <SC.DeleteButton type='button' onClick={handleInputDelete(index)}>
+                      -
+                    </SC.DeleteButton>
+                  )}
+                </SC.InputWrapper>
+              </SC.NameMeanContainer>
+            );
+          })}
           <SC.AddInputButton type='button' onClick={handleInputAdd}>
             의미 추가
           </SC.AddInputButton>
