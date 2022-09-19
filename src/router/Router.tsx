7ec89c2406731e391a88d5ce93a642cc3d05d24c
Circlewee <pnj1118@naver.com>
2022-09-19 17:57:41 +0900

    feat: connect to GA

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,18 +4,21 @@ import { NamePage } from '../pages/NamePage';
 import { BirthdayPage } from '../pages/BirthdayPage';
 import { ResultPage } from '../pages/ResultPage';
 import { Template } from '../pages/Template';
+import RouteChangeTracker from './RouteChangeTracker';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<Template />}>
-          <Route path='name' element={<NamePage />} />
-          <Route path='birthday' element={<BirthdayPage />} />
-          <Route path='*' element={<Navigate to='/name' />} />
-        </Route>
-        <Route path='/result' element={<ResultPage />} />
-      </Routes>
+      <RouteChangeTracker>
+        <Routes>
+          <Route path='/*' element={<Template />}>
+            <Route path='name' element={<NamePage />} />
+            <Route path='birthday' element={<BirthdayPage />} />
+            <Route path='*' element={<Navigate to='/name' />} />
+          </Route>
+          <Route path='/result' element={<ResultPage />} />
+        </Routes>
+      </RouteChangeTracker>
     </BrowserRouter>
   );
 };
